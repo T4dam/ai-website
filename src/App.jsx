64820bf4479365f84076e7cc1a678f1d713658e9
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-08-23 09:03:25 +0300

    components

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './style';
+import {
+	Billing,
+	FeedbackCard,
+	Business,
+	CardDeal,
+	Clients,
+	Hero,
+	Stats,
+	Navbar,
+	Footer,
+	CTA,
+	GetStarted,
+	Testimonials,
+} from './components';
+
+const App = () => (
+	<>
+		<div className="bg-primary w-full overflow-hidden">
+			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth}`}>
+					<Navbar />
+				</div>
+			</div>
+		</div>
+		<div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
+		</div>
+		<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>Stats Billing</div>
+		</div>
+	</>
+);
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,27 @@
+import Billing from './billing';
+import FeedbackCard from './feedback-card';
+import Business from './business';
+import CardDeal from './/card-deal';
+import Clients from './clients';
+import Hero from './hero';
+import Stats from './stats';
+import Navbar from './navbar';
+import Footer from '.footer';
+import CTA from './cta';
+import GetStarted from './get-started';
+import Testimonials from './testimonials';
+
+export {
+	Billing,
+	FeedbackCard,
+	Business,
+	CardDeal,
+	Clients,
+	Hero,
+	Stats,
+	Navbar,
+	Footer,
+	CTA,
+	GetStarted,
+	Testimonials,
+};
