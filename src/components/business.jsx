8ef563c523a37d3a9b8f748409603f4b02f391d2
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-08-25 12:52:46 +0300

    business section

--- src/components/business.jsx
+++ src/components/business.jsx
@@ -3,6 +3,30 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './button';
 
+const FeatureCards = ({ title, content, icon, index }) => {
+	return (
+		<div
+			className={`flex flex-row p-6 rounded-[20px] ${
+				index !== features.length - 1 ? 'mb-6' : 'mb-0'
+			} feature-card`}
+		>
+			<div
+				className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+			>
+				<img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+			</div>
+			<div className="flex flex-1 flex-col ml-3 justify-center">
+				<h4 className=" leading-[23px] font-semibold text-white text-[18px] font-poppins">
+					{title}
+				</h4>
+				<p className="leading-[23px] font-normal text-dimWhite text-[16px] font-poppins">
+					{content}
+				</p>
+			</div>
+		</div>
+	);
+};
+
 const Business = () => {
 	return (
 		<section id="features" className={layout.section}>
@@ -20,10 +44,10 @@ const Business = () => {
 				<Button styles="mt-10" text="Get started" />
 			</div>
 			<div className={`${layout.sectionImg} flex-col`}>
-                {features.map((item, index) =>(
-                    
-                ))}
-            </div>
+				{features.map((item, index) => (
+					<FeatureCards key={item.id} {...item} index={index} />
+				))}
+			</div>
 		</section>
 	);
 };
