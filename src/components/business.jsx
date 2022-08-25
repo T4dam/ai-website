307c6787abbb28ac36aa55c4c457cc7960cc0f7a
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-08-25 11:04:43 +0300

    button

--- src/components/business.jsx
+++ src/components/business.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { features } from '../constants';
+import styles, { layout } from '../style';
+import Button from './button';
 
 const Business = () => {
-	return <div>Business</div>;
+	return (
+		<section id="features" className={layout.section}>
+			<div className={layout.sectionInfo}>
+				<h2 className={styles.heading2}>
+					Some kick ass title <br className="hidden sm:block" />
+					to rock the world
+				</h2>
+				<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolores
+					quisquam explicabo, soluta illo, obcaecati ratione pariatur amet
+					culpa, asperiores nulla? Tempora quisquam eius totam vero iste quos
+					provident mollitia.
+				</p>
+				<Button styles="mt-10" text="Get started" />
+			</div>
+			<div className={`${layout.sectionImg} flex-col`}>
+                {features.map((item, index) =>(
+                    
+                ))}
+            </div>
+		</section>
+	);
 };
 
 export default Business;
