f521cf60f85f8119b36b00fd8f322bcdbc94afb2
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-09-05 21:50:40 +0300

    card css

--- src/components/howItWorksCard.jsx
+++ src/components/howItWorksCard.jsx
@@ -10,7 +10,6 @@ const HowItWorksCard = ({ id, title, icon, discription }) => {
 	const rotateX = useTransform(y, [-100, 100], [30, -30]);
 	const rotateY = useTransform(x, [-100, 100], [-30, 30]);
 
-	console.log(title);
 	return (
 		<div className="w-100 perspetive flex items-center justify-center">
 			<motion.div
@@ -19,12 +18,12 @@ const HowItWorksCard = ({ id, title, icon, discription }) => {
 				dragElastic={0.16}
 				dragConstraints={{ top: 0, right: 0, left: 0, bottom: 0 }}
 				style={{ x, y, rotateX, rotateY, z: 100 }}
-				className="w-[285px] h-[500px] flex flex-col rounded-[25px] shadow-lg cards  relative cursor-grab"
+				className="w-[285px] h-[500px] flex flex-col rounded-[25px] shadow-lg cards relative cursor-grab"
 			>
-				<div className="w-full flex flex-col flex-1 relative items-center justify-end py-[1em] px-[15px]">
-					<div className=" absolute  top-0 right-0 min-w-[100%] overflow-hidden max-w-[100%]  rounded-tr-[25px]">
-						<div className="absolute w-[350px] h-[350px] top-[-4.2em] right-[-10em] z-[5] bg-green-500 rounded-[50%]"></div>
-					</div>
+				<div className="w-full flex flex-col flex-[1.2] relative items-center justify-end py-[1em] px-[15px]">
+					{/* <div className=" absolute top-0 left-0 w-[100%] overflow-hidden h-[100%]  rounded-tr-[25px]">
+						<div className="absolute w-[350px] h-[350px] top-[-4.2em] right-[-10em] z-[5] bg-[#24a2b5] rounded-[50%]"></div>
+					</div> */}
 					<div className="w-full h-full absolute flex items-center justify-center">
 						<motion.div
 							style={{ x, y, rotateX, rotateY, z: 100000 }}
@@ -35,18 +34,22 @@ const HowItWorksCard = ({ id, title, icon, discription }) => {
 							className="w-auto h-[190px] z-[99] userselect-none "
 						>
 							<img
-								src="/logo.svg"
+								src={icon}
 								alt=""
 								unselectable="on"
 								className="w-auto h-full userselect-none"
 							/>
 						</motion.div>
 					</div>
-					<h2 className="m-none font-semibold text-2xl font-bold text-white font-poppins ">
-						HELLO THERE
+					<h2 className="m-none font-semibold text-2xl font-bold text-white z-[10] font-poppins ">
+						{title}
 					</h2>
 				</div>
-				<div className="flex flex-1 px-[1em]">bottim</div>
+				<div className="flex flex-1 px-[1em]">
+					<p className="font-poppins font-normal text-[20px] md:text-[16px] leading-[24px] text-dimWhite max-w-[300px]">
+						{discription}
+					</p>
+				</div>
 			</motion.div>
 		</div>
 	);
