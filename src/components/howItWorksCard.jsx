a4190d8cd87c10d35a641d04deb730209bf753b4
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-09-06 13:06:33 +0300

    resize cards

--- src/components/howItWorksCard.jsx
+++ src/components/howItWorksCard.jsx
@@ -20,7 +20,7 @@ const HowItWorksCard = ({ id, shadow, title, icon, discription }) => {
 				dragElastic={0.16}
 				dragConstraints={{ top: 0, right: 0, left: 0, bottom: 0 }}
 				style={{ x, y, rotateX, rotateY, z: 100 }}
-				className="w-[285px] h-[500px] flex flex-col rounded-[25px] shadow-lg cards relative cursor-grab"
+				className=" w-full md:w-[285px] h-[500px] xs:h-[265px] md:h-[500px] flex flex-row md:flex-col rounded-[25px] shadow-lg cards relative cursor-grab"
 			>
 				<div className="w-full flex flex-col flex-[1.2] relative items-center justify-end py-[1em] px-[15px]">
 					<div className=" absolute top-0 left-0 w-[100%] overflow-hidden h-[100%]  rounded-tr-[25px]">
@@ -69,16 +69,20 @@ const HowItWorksCard = ({ id, shadow, title, icon, discription }) => {
 							/>
 						</motion.div>
 					</div>
-					<h2 className="m-none font-semibold text-2xl font-bold text-white z-[10] font-poppins ">
+					<h2 className="hidden md:block m-none font-semibold text-2xl font-bold text-white z-[10] font-poppins ">
 						{title}
 					</h2>
 				</div>
-				<div className="flex flex-[0.8] w-full h-full flex-col px-[1em] pb-[3em]">
+				<div className="flex flex-[0.8] w-full h-full flex-col justify-center gap-1  px-[1em] md:pb-[3em]">
+					<h2 className="md:hidden  m-none font-semibold text-2xl font-bold text-white z-[10] font-poppins ">
+						{title}
+					</h2>
+
 					<p className="font-poppins font-normal text-[20px] md:text-[16px] leading-[24px] text-dimWhite max-w-[300px]">
 						{discription}
 					</p>
-					<div className=" inline-block h-full flex flex-col justify-end">
-						<a className=" block w-[190px] md:w-[130px] flex flex-row  items-center text-white font-poppins font-normal text-[20px] md:text-[16px] leading-[24px] text-dimWhite cursor-pointer ">
+					<div className=" inline-block md:h-full flex flex-col md:justify-end">
+						<a className=" mt-2 md:mt-0 block w-[190px] md:w-[130px] flex flex-row  items-center text-white font-poppins font-normal text-[20px] md:text-[16px] leading-[24px] text-dimWhite cursor-pointer ">
 							Learn more
 							<div className=" inline-block flex justify-center items-center ml-2 w-[20px] h-[20px] bg-[#00F6FF]  rounded-[50%]">
 								<GrFormNext />
